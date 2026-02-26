c4a22cf4357657c82ec0e00fadb1160b875e77be
jadathibodeaux <jada.r.thibodeaux@gmail.com>
2026-02-25 20:25:25 -0600

    moved the files due to error *lmao*

--- main/java/com/jada/ecommerce/EcommerceApplication.java
+++ main/java/com/jada/ecommerce/EcommerceApplication.java
@@ -1,4 +1,4 @@
-package com.example.ecommerce;
+package com.jada.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
