8ca532df4879b1a70cf6b325022118ef5e43bd64
jadathibodeaux <jada.r.thibodeaux@gmail.com>
2026-03-01 16:43:56 -0600

    product service

--- main/java/com/jada/ecommerce/service/ProductService.java
+++ main/java/com/jada/ecommerce/service/ProductService.java
@@ -9,21 +9,17 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    //CONSTRUCTOR INJECT REPOSITORY INTO SERVICE
     private final ProductRepository productRepository;
 
-    //CONSTRUCTOR TO INJECT
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    //RETURN LIST
-    public List<Product> getAllProducts (){
-        //RETURN IT WITH THE REPOSITORY METHOD
-        return productRepository.findAll();
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
     }
 
-    public Product createProduct(Product product) {
-        return productRepository.save(product);
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 }
