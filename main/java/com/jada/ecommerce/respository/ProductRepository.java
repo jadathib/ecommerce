8220ee42913e4224e1f6292955b3dc29a2b99f03
jadathibodeaux <jada.r.thibodeaux@gmail.com>
2026-03-01 15:39:17 -0600

    turned repository to an interface

--- main/java/com/jada/ecommerce/respository/ProductRepository.java
+++ main/java/com/jada/ecommerce/respository/ProductRepository.java
@@ -1,24 +1,8 @@
 package com.jada.ecommerce.respository;
 
 import com.jada.ecommerce.model.Product;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.List;
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
-@Repository
-public class ProductRepository {
-    //NEW LIST OF PRODUCTS
-    private final List<Product> products = new ArrayList<>();
-
-    //CONSTRUCTOR TO ADD PRODUCTS
-    public ProductRepository() {
-        products.add(new Product(1L, "Laptop", 1200.00, 10));
-        products.add(new Product(2L, "Phone", 800.00, 15));
-    }
-
-    //RETURN THE LIST OF THE PRODUCTS
-    public List<Product> findAll() {
-        return products;
-    }
 }
