9ca66b8c868f29296f3f0d503b37565c5efcf320
jadathibodeaux <jada.r.thibodeaux@gmail.com>
2026-03-01 16:02:13 -0600

    fixed constructor

--- main/java/com/jada/ecommerce/model/Product.java
+++ main/java/com/jada/ecommerce/model/Product.java
@@ -14,19 +14,15 @@ public class Product {
 
     private String name;
     private double price;
-    private int stockQuantity;
 
     // constructor
-    public Product(Long id, String name, double price, int stockQuantity) {
-        this.id = id;
+    public Product(String name, double price) {
         this.name = name;
         this.price = price;
-        this.stockQuantity = stockQuantity;
     }
 
     // getters
     public Long getId() { return id; }
     public String getName() { return name; }
     public double getPrice() { return price; }
-    public int getStockQuantity() { return stockQuantity; }
 }
