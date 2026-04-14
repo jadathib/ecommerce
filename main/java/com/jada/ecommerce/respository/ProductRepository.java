f5e27fd0a86d946ee261edf41637a618902e3114
jadathibodeaux <jada.r.thibodeaux@gmail.com>
2026-04-13 21:19:21 -0500

    Just comment

--- main/java/com/jada/ecommerce/respository/ProductRepository.java
+++ main/java/com/jada/ecommerce/respository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.jada.ecommerce.respository;
 import com.jada.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//ORM BASED SQL COMMANDS
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
