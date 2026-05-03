84c6c52fb8ccfcf34737edd870a1274ad3569406
jadathibodeaux <jada.r.thibodeaux@gmail.com>
2026-05-03 01:32:35 -0500

    Added findById or through exception

--- main/java/com/jada/ecommerce/service/ProductService.java
+++ main/java/com/jada/ecommerce/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.jada.ecommerce.service;
 
 import com.jada.ecommerce.config.AppConfig;
+import com.jada.ecommerce.exception.ResourceNotFoundException;
 import com.jada.ecommerce.model.Product;
 import com.jada.ecommerce.respository.ProductRepository;
 import org.springframework.stereotype.Service;
@@ -27,7 +28,10 @@ public class ProductService {
     }
 
     public Product getProductById(Long id) {
-        return productRepository.findById(id).orElse(null);
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Product not found with id: " + id
+                ));
     }
 
     //VOID BECAUSE WE ARE DELETING AND NOT RETURNING ANYTHING
