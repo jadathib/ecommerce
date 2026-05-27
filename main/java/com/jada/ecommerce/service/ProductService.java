b15e45d238744d65c934ad2181822b3a0e089a89
jadathibodeaux <jada.r.thibodeaux@gmail.com>
2026-05-26 21:06:25 -0500

    refactored repository name

--- main/java/com/jada/ecommerce/service/ProductService.java
+++ main/java/com/jada/ecommerce/service/ProductService.java
@@ -3,7 +3,9 @@ package com.jada.ecommerce.service;
 import com.jada.ecommerce.config.AppConfig;
 import com.jada.ecommerce.exception.ResourceNotFoundException;
 import com.jada.ecommerce.model.Product;
-import com.jada.ecommerce.respository.ProductRepository;
+import com.jada.ecommerce.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +13,7 @@ import java.util.List;
 @Service
 public class ProductService {
 
+    private static final Logger log = LoggerFactory.getLogger(ProductService.class);
     private final ProductRepository productRepository;
     private final AppConfig appConfig;
 
@@ -42,5 +45,24 @@ public class ProductService {
     //TAX CALC
     public double calculatePriceWithTax(double price) {
         return price + (price * appConfig.getTaxRate());
+    }
+
+    //FIND BY PRICE
+    public List<Product> getProductByPrice (double price) {
+        return productRepository.findByPrice(price);
+    }
+
+    //FIND BY NAME
+    public List<Product> getProductByName(String name) {
+        //CALL THE REPOSITORY OF THE PRODUCTS
+        List<Product> products = productRepository.findByNameContainingIgnoreCase(name);
+
+        //IF YOU CAN'T FIND ANY
+        if (products.isEmpty()) {
+            //THROW THE EXCEPTION
+            throw new ResourceNotFoundException("No products found containing: " + name);
+        }
+        //RETURN THE PRODUCTS
+        return products;
     }
 }
