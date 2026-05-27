2c1acbdb81baab68eabdff49fdd71c0d3390b4a1
jadathibodeaux <jada.r.thibodeaux@gmail.com>
2026-05-26 21:06:11 -0500

    added the findbyPrice and findByNameContainingIgnoreCases custom methods to the repository

--- main/java/com/jada/ecommerce/repository/ProductRepository.java
+++ main/java/com/jada/ecommerce/repository/ProductRepository.java
@@ -1,9 +1,13 @@
-package com.jada.ecommerce.respository;
+package com.jada.ecommerce.repository;
 
 import com.jada.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 //ORM BASED SQL COMMANDS
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    public List<Product> findByPrice(double price);
 
+    public List<Product> findByNameContainingIgnoreCase(String name);
 }
