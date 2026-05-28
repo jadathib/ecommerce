2929147461dba3b5d110cfb32b46f1e7faaf02f7
jadathibodeaux <jada.r.thibodeaux@gmail.com>
2026-05-27 23:57:02 -0500

    Added findByNameIgnoreCase

--- main/java/com/jada/ecommerce/repository/CategoryRepository.java
+++ main/java/com/jada/ecommerce/repository/CategoryRepository.java
@@ -1,10 +1,12 @@
 package com.jada.ecommerce.repository;
 
 import com.jada.ecommerce.model.Category;
+import com.jada.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository <Category, Long> {
-
+    Optional<Category> findByNameIgnoreCase(String name);
 }
