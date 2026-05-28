919b72bd2e1f332871fc7cf91e0fd6a07d7ba93e
jadathibodeaux <jada.r.thibodeaux@gmail.com>
2026-05-27 21:24:15 -0500

    Category repository

---
+++ main/java/com/jada/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.jada.ecommerce.repository;
+
+import com.jada.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository <Category, Long> {
+
+}
